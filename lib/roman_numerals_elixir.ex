365ad3530d50860f1e75af424430fc6b0bfcea5e
iceburg333 <ice_burg333@yahoo.com>
2019-02-08 19:56:15 -0500

    working except for the nines

--- lib/roman_numerals_elixir.ex
+++ lib/roman_numerals_elixir.ex
@@ -1,21 +1,30 @@
 defmodule RomanNumeralsElixir do
+  @numerals [
+    {1000, "M"},
+    {500, "D"},
+    {100, "C"},
+    {50, "L"},
+    {10, "X"},
+    {5, "V"},
+    {1, "I"}
+  ]
   def convert(number, letters \\ "") do
-    # letters = convert_letters(rawLetters)
+    {number, numerals} = Enum.reduce(@numerals, {number, ""}, &reduce_thing/2)
+    replace_nines(numerals)
+  end
 
+  defp reduce_thing({numeralValue, numeralSign}, {number, letters}) do
+    IO.inspect("numeral: #{numeralValue}, number: #{number}")
     cond do
-      number >= 1000 -> convert(number - 1000, letters <> "M")
-      number >= 500 -> convert(number - 500, letters <> "D")
-      number >= 100 -> convert(number - 100, letters <> "C")
-      number >= 90 -> convert(number - 90, letters <> "XC")
-      number >= 50 -> convert(number - 50, letters <> "L")
-      number >= 40 -> convert(number - 40, letters <> "XL")
-      number >= 10 -> convert(number - 10, letters <> "X")
-      number >= 9 -> convert(number - 9, letters <> "IX")
-      number >= 5 -> convert(number - 5, letters <> "V")
-      number >= 4 -> convert(number - 4, letters <> "IV")
-      number >= 1 -> convert(number - 1, letters <> "I")
-      true -> letters
+      number >= numeralValue -> {rem(number, numeralValue), letters <> String.duplicate(numeralSign, div(number, numeralValue))}
+      true -> {number, letters}
     end
+  end
+
+  defp replace_nines(numerals) do
+    numerals
+    |> String.replace("IIII", "IV")
+    |> String.replace("XXXX", "XL")
   end
 
 end
